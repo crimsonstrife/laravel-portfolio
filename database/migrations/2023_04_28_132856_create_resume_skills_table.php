8441c2d49cc02c46262d594769c12adca4771174
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-06-28 12:55:13 -0400

    Create 2023_04_28_132856_create_resume_skills_table.php

---
+++ database/migrations/2023_04_28_132856_create_resume_skills_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resume_skills', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('skill')->unique();
+            $table->float('level')->min(0)->max(100)->default(0);
+            $table->string('description')->nullable();
+            $table->string('slug')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resume_skills');
+    }
+};
