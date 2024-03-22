4e404bb661f1ca7a7b70228b5faeb646635dd03f
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-03-22 15:13:39 -0400

    Update open-admin-form.js

--- public/vendor/open-admin/open-admin/js/open-admin-form.js
+++ public/vendor/open-admin/open-admin/js/open-admin-form.js
@@ -19,7 +19,7 @@ admin.form = {
     },
 
     beforeSave: function () {
-        for (i in this.beforeSaveCallbacks) {
+        for (let i in this.beforeSaveCallbacks) {
             var callback = this.beforeSaveCallbacks[i];
             callback();
         }
@@ -27,29 +27,32 @@ admin.form = {
 
     addAjaxSubmit: function () {
         // forms that should be submitted with ajax
-        Array.from(document.getElementsByTagName('form')).forEach((form) => {
-            if (form.getAttribute('pjax-container') != null && !form.classList.contains('has-ajax-handler')) {
-                form.addEventListener('submit', function (event) {
+        Array.from(document.getElementsByTagName("form")).forEach((form) => {
+            if (
+                form.getAttribute("pjax-container") != null &&
+                !form.classList.contains("has-ajax-handler")
+            ) {
+                form.addEventListener("submit", function (event) {
                     admin.form.submit(event.target);
 
                     event.preventDefault();
                     return false;
                 });
 
-                form.classList.add('has-ajax-handler');
+                form.classList.add("has-ajax-handler");
             }
         });
     },
 
     submit: function (form, result_function) {
-        let method = form.getAttribute('method').toLowerCase();
-        let url = String(form.getAttribute('action')).split('?')[0];
+        let method = form.getAttribute("method").toLowerCase();
+        let url = String(form.getAttribute("action")).split("?")[0];
         let obj = {};
 
         this.beforeSave();
 
         if (admin.form.validate(form)) {
-            if (method === 'post' || method === 'put') {
+            if (method === "post" || method === "put") {
                 obj.data = new FormData(form);
                 obj.method = method;
             } else {
@@ -57,29 +60,33 @@ admin.form = {
                 let data = new FormData(form);
                 let searchParams = new URLSearchParams(data);
                 let query_str = searchParams.toString();
-                url += '?' + query_str;
+                url += "?" + query_str;
 
-                if (typeof result_function !== 'function') {
+                if (typeof result_function !== "function") {
                     admin.ajax.setUrl(url);
                 }
             }
 
-            if (typeof result_function === 'function') {
+            if (typeof result_function === "function") {
                 admin.ajax.request(url, obj, result_function);
             } else {
                 admin.ajax.load(url, obj);
             }
         } else {
-            console.log('Form still has errors');
+            console.log("Form still has errors");
         }
     },
 
     footer: function () {
-        document.querySelectorAll('.after-submit').forEach((check) => {
-            check.addEventListener('click', function () {
-                document.querySelectorAll(".after-submit:not([value='" + this.value + "']").forEach((other) => {
-                    other.checked = false;
-                });
+        document.querySelectorAll(".after-submit").forEach((check) => {
+            check.addEventListener("click", function () {
+                document
+                    .querySelectorAll(
+                        ".after-submit:not([value='" + this.value + "']"
+                    )
+                    .forEach((other) => {
+                        other.checked = false;
+                    });
             });
         });
     },
@@ -87,16 +94,18 @@ admin.form = {
     tabs: function () {
         var hash = document.location.hash;
         if (hash) {
-            var activeTab = document.querySelector('.nav-tabs a[href="' + hash + '"]');
+            var activeTab = document.querySelector(
+                '.nav-tabs a[href="' + hash + '"]'
+            );
             if (activeTab) {
                 new bootstrap.Tab(activeTab).show();
             }
         }
 
-        this.tabs_ref = document.querySelectorAll('.nav-tabs');
+        this.tabs_ref = document.querySelectorAll(".nav-tabs");
         if (this.tabs_ref.length) {
             this.tabs_ref.forEach((tab) => {
-                tab.addEventListener('shown.bs.tab', function (event) {
+                tab.addEventListener("shown.bs.tab", function (event) {
                     // replaceState insted of pushSt (prevents tab navigation from going into the history)
                     history.replaceState(null, null, event.target.hash);
                 });
@@ -106,39 +115,49 @@ admin.form = {
     },
 
     check_tab_errors() {
-        let errors = document.querySelectorAll('.tab-pane .has-error, .was-validated .tab-pane .form-control:invalid');
+        let errors = document.querySelectorAll(
+            ".tab-pane .has-error, .was-validated .tab-pane .form-control:invalid"
+        );
         if (this.tabs_ref.length && errors) {
             let first_tab = false;
             errors.forEach((error) => {
-                let tabId = '#' + error.closest('.tab-pane').getAttribute('id');
-                document.querySelector('li a[href="' + tabId + '"] i').classList.remove('hide');
+                let tabId = "#" + error.closest(".tab-pane").getAttribute("id");
+                document
+                    .querySelector('li a[href="' + tabId + '"] i')
+                    .classList.remove("hide");
                 if (!first_tab) {
                     first_tab = tabId;
                 }
             });
             if (first_tab) {
-                let errorTab = document.querySelector('.nav-tabs a[href="' + first_tab + '"]');
+                let errorTab = document.querySelector(
+                    '.nav-tabs a[href="' + first_tab + '"]'
+                );
                 new bootstrap.Tab(errorTab).show();
             }
         }
     },
 
     disable_cascaded_forms: function (selector) {
-        document.querySelector(selector).addEventListener('submit', function (event) {
-            let elems = event.target.querySelectorAll('div.cascade-group.d-none input');
-            if (elems) {
-                elems.forEach((field) => {
-                    field.setAttribute('disabled', true);
-                });
-            }
-        });
+        document
+            .querySelector(selector)
+            .addEventListener("submit", function (event) {
+                let elems = event.target.querySelectorAll(
+                    "div.cascade-group.d-none input"
+                );
+                if (elems) {
+                    elems.forEach((field) => {
+                        field.setAttribute("disabled", true);
+                    });
+                }
+            });
     },
 
     initValidation: function () {
-        var forms = document.querySelectorAll('.needs-validation');
+        var forms = document.querySelectorAll(".needs-validation");
         forms.forEach(function (form) {
             form.addEventListener(
-                'submit',
+                "submit",
                 function (event) {
                     if (!admin.form.validate(form)) {
                         event.preventDefault();
@@ -154,9 +173,9 @@ admin.form = {
     validate: function (form) {
         let res = true;
 
-        if (form.classList.contains('needs-validation')) {
+        if (form.classList.contains("needs-validation")) {
             res = form.checkValidity();
-            form.classList.add('was-validated');
+            form.classList.add("was-validated");
             admin.form.check_tab_errors();
         }
         return res;
