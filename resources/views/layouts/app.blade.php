78b44b049d6dfe34b362f4a0ca56bfbadb0c1a15
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-04-26 15:49:33 -0400

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Laravel Assets -->
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @routes
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+    <div class="container-fluid">
+
+        <div class="row">
+
+            @yield('main-row')
+
+        </div>
+
+    </div>
+</body>
+
+</html>
