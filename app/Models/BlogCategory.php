e155ca21f3454a08af6fa840f475ad17ecb4e768
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-04-26 15:49:23 -0400

    Update BlogCategory.php

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -9,8 +9,20 @@ class BlogCategory extends Model
 {
     use HasFactory;
 
+    //table name
+    protected $table = 'blog_category';
+
     //has names and slugs
     protected $fillable = ['name', 'slug'];
+
+    //has timestamps
+    public $timestamps = true;
+
+    //has id
+    protected $primaryKey = 'id';
+
+    //has author
+    protected $author = 'author';
 
     //has many posts
     public function posts()
