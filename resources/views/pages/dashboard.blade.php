834397177f9ce0139765c4b1c34dc4ead7ae6e82
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-04-26 15:49:36 -0400

    Update dashboard.blade.php

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,10 @@
+<!-- Extending from the App Layout -->
+@extends('layouts.app')
+
+<!-- Main Content -->
+@section('main-row')
+    <!-- Include the Sidebar Content -->
+    @include('inc.sideArea')
+
+    <!-- Include the Main Content -->
+@endsection
